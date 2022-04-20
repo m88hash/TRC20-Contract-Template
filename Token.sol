f3bd60d23feb8dc7f85a68a6ef118f1c72c1eb18
m88hash <104025103+m88hash@users.noreply.github.com>
2022-04-20 14:51:46 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("M88HASH.COM", "M88HASH", 18) {
+    constructor () public TRC20Detailed("M88HASH.com", "M88HASH.com", 18) {
         _mint(msg.sender, 10000000000000 * (10 ** uint256(decimals())));
     }
 }
